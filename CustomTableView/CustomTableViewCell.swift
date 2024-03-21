9e2f005fc6fff3b3cac81b30979f550d5fa8e0d7
Francisco Diaz Roldan <francisco.diazro@viewnet.com>
2024-03-21 06:46:32 -0700

    Ejercicio completo

---
+++ CustomTableView/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  CustomTableView
+//
+//  Created by Francisco Díaz Roldán on 20/3/24.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+
+}
--- CustomTableView/ViewController.swift
+++ CustomTableView/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var table: UITableView!
 
+    struct Paisaje {
+        let title: String
+        let imageName: String
+    }
+    
+    let data: [Paisaje] = [
+        Paisaje(title: "Karim Benzema", imageName: "Benzema"),
+        Paisaje(title: "Imagen 2", imageName: "chilkoottrail"),
+        Paisaje(title: "Imagen 3", imageName: "chincoteague"),
+        Paisaje(title: "Imagen 4", imageName: "hiddenlake"),
+        Paisaje(title: "Imagen 5", imageName: "icybay"),
+        Paisaje(title: "Imagen 6", imageName: "lakemcdonald"),
+        Paisaje(title: "Imagen 7", imageName: "rainbowlake"),
+        Paisaje(title: "Imagen 8", imageName: "silversalmoncreek"),
+        Paisaje(title: "Imagen 9", imageName: "stmarylake"),
+        Paisaje(title: "Imagen 10", imageName: "twinlake"),
+        Paisaje(title: "Imagen 11", imageName: "umbagog"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.dataSource = self
+        table.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let paisaje = data[indexPath.row]
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        cell.label.text = paisaje.title
+        cell.iconImageView.image = UIImage(named: paisaje.imageName)
+        return cell
+    }
 
+    
+    func tableView(_ tableView: UITableView,  heightForRowAt indexPath: IndexPath)  ->
+        CGFloat {
+            return 140
+    }
 }
-
